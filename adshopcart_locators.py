09f5422f1d13f42098404b00a30321c858dda804
alvienj <99428780+alvienj@users.noreply.github.com>
2022-03-03 12:09:13 -0800

    Update adshopcart_locators.py

--- adshopcart_locators.py
+++ adshopcart_locators.py
@@ -2,7 +2,8 @@ from faker import Faker
 
 fake = Faker(locale='en_CA')
 adshopcart_url = 'https://advantageonlineshopping.com/#/'
-adshopcart_username = fake.user_name()
+old_adshopcart_username = fake.user_name()
+new_adshopcart_username = old_adshopcart_username[0:15]
 adshopcart_email = fake.email()
 adshopcart_password = fake.password()
 adshopcart_firstname = fake.first_name()
