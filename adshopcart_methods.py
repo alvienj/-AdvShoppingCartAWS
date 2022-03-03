ce1c5e392807cf1396ce83f0636cda0c4f3643db
alvienj <99428780+alvienj@users.noreply.github.com>
2022-03-03 12:11:51 -0800

    Update adshopcart_methods.py

--- adshopcart_methods.py
+++ adshopcart_methods.py
@@ -1,4 +1,4 @@
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 import adshopcart_locators as locators
@@ -7,8 +7,17 @@ from time import sleep
 import datetime
 import sys
 
-s = Service('C:/Users/User/PycharmProjects/si/advantage_shopping_cart/chromedriver.exe')
-driver = webdriver.Chrome(service=s)
+options = Options()
+options.add_argument("--headless")
+options.add_argument("window-size=1400,1500")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("start-maximized")
+options.add_argument("enable-automation")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(options=options)
 
 
 def setUp():
